800bcc9967f8a3a37343f4ab2c98f1b846a8edb7
Farah Juma <fjuma@redhat.com>
2019-08-27 14:15:32 -0400

    [WFCORE-4150] Add the ability for a credential-reference to handle both a credential store reference and a clear text password at the same time to allow for automatic updates to credential stores

--- controller/src/main/java/org/jboss/as/controller/logging/ControllerLogger.java
+++ controller/src/main/java/org/jboss/as/controller/logging/ControllerLogger.java
@@ -3555,4 +3555,7 @@ public interface ControllerLogger extends BasicLogger {
     @LogMessage(level = WARN)
     @Message(id = 456, value = "System property %s=%s already set. It's being overridden by new value %s")
     void systemPropertyAlreadyExist(String name, String value, String value2);
+
+    @Message(id = 457, value = "Value for attribute '%s' is invalid.")
+    OperationFailedException invalidAttributeValue(String attributeName);
 }
